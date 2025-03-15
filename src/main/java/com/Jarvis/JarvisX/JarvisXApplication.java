035215fe07c4df3b45c4898d5722761d28369f82
csLamar12 <90802465+csLamar12@users.noreply.github.com>
2025-03-15 13:55:12 -0500

    initialization

---
+++ src/main/java/com/Jarvis/JarvisX/JarvisXApplication.java
@@ -0,0 +1,13 @@
+package com.Jarvis.JarvisX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarvisXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarvisXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jarvis/JarvisX/JarvisXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jarvis.JarvisX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JarvisXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
